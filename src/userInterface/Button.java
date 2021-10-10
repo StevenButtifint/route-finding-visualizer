9802b50e8c13c24cf47e9811b79d23ac8fb613e7
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-10-10 22:09:06 +0100

    added button class getters and setters

--- src/userInterface/Button.java
+++ src/userInterface/Button.java
@@ -27,4 +27,51 @@ public class Button {
 		this.height = texture.getImageHeight();
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Texture getTexture() {
+		return texture;
+	}
+
+	public void setTexture(Texture texture) {
+		this.texture = texture;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
 }
