6842bfb8e39bf37a50aef64d376018ad51b733ee
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-10-11 19:05:15 +0100

    added Artist class session initialize

---
+++ src/helpers/Artist.java
@@ -0,0 +1,44 @@
+package helpers;
+
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glOrtho;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
+
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class Artist {
+	
+	public static final int WIDTH = 1280, HEIGHT = 720;
+			
+	public static void InitializeSession() {
+		Display.setTitle("Route Finding Visualizer");
+		try {
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, WIDTH, HEIGHT, 0, 1, -1);
+		glMatrixMode(GL_MODELVIEW);
+		glEnable(GL_TEXTURE_2D);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	}
+	
+}
