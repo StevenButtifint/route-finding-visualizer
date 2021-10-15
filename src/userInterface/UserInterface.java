7bff57a326c60074f9066d1829a3cfd046d7ee49
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-10-15 23:27:52 +0100

    added UserInterface class and constructor

---
+++ src/userInterface/UserInterface.java
@@ -0,0 +1,19 @@
+package userInterface;
+
+import java.util.ArrayList;
+
+import org.lwjgl.input.Mouse;
+import static helpers.Artist.*;
+
+
+public class UserInterface {
+	
+	private ArrayList<Button> buttonList;
+	
+	
+	public UserInterface() {
+		buttonList = new ArrayList<Button>();
+	}
+	
+
+}
