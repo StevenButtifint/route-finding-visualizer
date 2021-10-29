b9bbed6adbbb443a874f182806805e885d599d3c
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-10-29 18:18:37 +0100

    added clear search function

--- src/data/SearchAnimator.java
+++ src/data/SearchAnimator.java
@@ -24,5 +24,36 @@ public class SearchAnimator {
 	}
 	
 	
+	
+	
+	public TileGrid clearSearch() {
+		for (int x = 0; x<grid.getTilesX(); x++) {
+			for (int y = 0; y<grid.getTilesY(); y++) {
+				TileType tileType = grid.getTileType(x, y);	
+				switch(tileType) {
+					case BLANK_TILE_SEARCHED:
+					case PATH_TILE:
+						this.grid.setTile(x, y, TileType.BLANK_TILE);
+						break;
+					case CHECKPOINT_TILE_SEARCHED:
+					case CHECKPOINT_TILE_COMPLETE:
+						this.grid.setTile(x, y, TileType.CHECKPOINT_TILE);
+						break;
+					case END_TILE_SEARCHED:
+					case END_TILE_COMPLETE:
+						this.grid.setTile(x, y, TileType.END_TILE);
+						break;
+					case START_TILE_SEARCHED:
+					case START_TILE_COMPLETE:
+						this.grid.setTile(x, y, TileType.START_TILE);
+						break;
+					default:
+						break;
+				}
+			}
+		}
+		return this.grid;
+	}
+	
 
 }
